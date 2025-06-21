46406a9040f1cc81bd361ea70e6cd61fa60b82e1
YehinnerTA <ytorresastorga@gmail.com>
2025-06-21 01:09:31 -0500

    Featured insert UI

--- src/adapters/ui/components/view/Product/FeaturedProduct_LandingPage.tsx
+++ src/adapters/ui/components/view/Product/FeaturedProduct_LandingPage.tsx
@@ -3,9 +3,24 @@ import './FeaturedProduct_LandingPage.css';
 
 const FeaturedProduct_LandingPage: React.FC = () => {
     return (
-        <div className="container">
-            <strong>Destacados</strong>
-            <p>Explore <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
+        <div className="container-Featured">
+            <h2 className="featured-title">Productos Favoritos</h2>
+            <p className="featured-description">¡Descubre todo lo que te encanta en un solo lugar!</p>
+
+            <div className="featured-products">
+                <div className="item-featured-product">
+                    <img src="/src/assets/images/apple-iphone13.png" alt="Producto Destacado" className="featured-product-image" />
+                    <h3 className="featured-product-title">Producto Destacado 1</h3>
+                    <p className="featured-product-price">$19.99</p>
+                    <button className="featured-product-button">Comprar</button>
+                </div>
+                <div className="item-featured-product">
+                    <img src="/src/assets/images/apple-iphone13.png" alt="Producto Destacado" className="featured-product-image" />
+                    <h3 className="featured-product-title">Producto Destacado 1</h3>
+                    <p className="featured-product-price">$19.99</p>
+                    <button className="featured-product-button">Comprar</button>
+                </div>
+            </div>
         </div>
     );
 };
