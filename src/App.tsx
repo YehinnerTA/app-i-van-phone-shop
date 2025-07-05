9035a8927a44a5b95da7cc32c1d0fef2ee2dc003
YehinnerTA <ytorresastorga@gmail.com>
2025-07-04 19:16:29 -0500

    Update...

--- src/App.tsx
+++ src/App.tsx
@@ -8,9 +8,9 @@ import { IonReactRouter } from '@ionic/react-router';
 import SplashLoader from './adapters/navigation/SplashLoader';
 import Login from './adapters/ui/pages/security/Login';
 import Register from './adapters/ui/pages/security/Register';
+
 import Home from './adapters/ui/pages/user/Home';
 import Payment from './adapters/ui/pages/user/Payment';
-
 import CatalogProduct from './adapters/ui/pages/user/CatalogProduct';
 import FeaturedProduct from './adapters/ui/pages/user/FeaturedProduct';
 import Profile from './adapters/ui/pages/user/Profile';
@@ -22,36 +22,19 @@ import DashboardClient from './adapters/ui/pages/admin/DashboardClient';
 import DashboardAnalysis from './adapters/ui/pages/admin/DashboardAnalysis';
 import DashboardInventory from './adapters/ui/pages/admin/DashboardInventory';
 
-
-/* Core CSS required for Ionic components to work properly */
 import '@ionic/react/css/core.css';
-
-/* Basic CSS for apps built with Ionic */
 import '@ionic/react/css/normalize.css';
 import '@ionic/react/css/structure.css';
 import '@ionic/react/css/typography.css';
-
-/* Optional CSS utils that can be commented out */
 import '@ionic/react/css/padding.css';
 import '@ionic/react/css/float-elements.css';
 import '@ionic/react/css/text-alignment.css';
 import '@ionic/react/css/text-transformation.css';
 import '@ionic/react/css/flex-utils.css';
 import '@ionic/react/css/display.css';
-
-/**
- * Ionic Dark Mode
- * -----------------------------------------------------
- * For more info, please see:
- * https://ionicframework.com/docs/theming/dark-mode
- */
-
-/* import '@ionic/react/css/palettes/dark.always.css'; */
-/* import '@ionic/react/css/palettes/dark.class.css'; */
 import '@ionic/react/css/palettes/dark.system.css';
-
-/* Theme variables */
 import './adapters/ui/theme/variables.css';
+
 setupIonicReact();
 
 const App: React.FC = () => (
@@ -62,10 +45,8 @@ const App: React.FC = () => (
         <Route path="/login" component={Login} />
         <Route path="/register" component={Register} />
 
-        {/* Rutas privadas con autenticación */}
         <Route path="/home" component={Home} />
         <Route path="/payment" component={Payment} />
-        
         <Route path="/catalogproduct" component={CatalogProduct} />
         <Route path="/featuredproduct" component={FeaturedProduct} />
         <Route path="/profile" component={Profile} />
@@ -76,7 +57,7 @@ const App: React.FC = () => (
         <Route path="/dashboard-client" component={DashboardClient} />
         <Route path="/dashboard-analysis" component={DashboardAnalysis} />
         <Route path="/dashboard-inventory" component={DashboardInventory} />
-        
+
         <Redirect to="/" />
       </IonRouterOutlet>
     </IonReactRouter>
